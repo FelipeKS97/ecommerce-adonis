fc2008618e7a5ef9cda9f6fc572637464ded5137
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-02-06 19:58:13 -0300

    adonis validator/websocket/acl installed and registered, some acl configs only left

--- app/Models/User.js
+++ app/Models/User.js
@@ -21,6 +21,13 @@ class User extends Model {
     })
   }
 
+  static get traits () {
+    return [
+      '@provider:Adonis/Acl/HasRole',
+      '@provider:Adonis/Acl/HasPermission'
+    ]
+  }
+
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
--- start/app.js
+++ start/app.js
@@ -16,7 +16,10 @@ const providers = [
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
-  '@adonisjs/mail/providers/MailProvider'
+  '@adonisjs/mail/providers/MailProvider',
+  '@adonisjs/validator/providers/ValidatorProvider',
+  '@adonisjs/websocket/providers/WsProvider',
+  'adonis-acl/providers/AclProvider'
 ]
 
 /*
@@ -29,7 +32,8 @@ const providers = [
 |
 */
 const aceProviders = [
-  '@adonisjs/lucid/providers/MigrationsProvider'
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  'adonis-acl/providers/CommandsProvider'
 ]
 
 /*
@@ -44,7 +48,10 @@ const aceProviders = [
 |   { Route: 'Adonis/Src/Route' }
 |
 */
-const aliases = {}
+const aliases = {
+  Role: 'Adonis/Acl/Role',
+  Permission: 'Adonis/Acl/Permission',
+}
 
 /*
 |--------------------------------------------------------------------------
