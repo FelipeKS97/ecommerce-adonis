cf182b820a6a4d1eb0a2b781bbf8002530e727d6
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-02-15 21:12:52 -0300

    New hook created: OrderHook, Order boot method implemented and hooks added

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -5,6 +5,13 @@ const Model = use('Model')
 
 class Order extends Model {
 
+    static boot() {
+        super.boot()
+
+        this.addHook('afterFind', 'OrderHook.updateValues')
+        this.addHook('afterPaginate', 'OrderHook.updateCollectionValues')
+    }
+
      // Relationship between Order and User
      user() {
         this.belongsTo('App/Models/User', 'user_id', 'id')
