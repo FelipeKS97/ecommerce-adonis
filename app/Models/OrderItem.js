2ebcafeec477b6ddb4813528f2904921ee73a8d9
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-02-17 16:06:42 -0300

    new hook implemented: OrderItemHook

--- app/Models/OrderItem.js
+++ app/Models/OrderItem.js
@@ -5,9 +5,17 @@ const Model = use('Model')
 
 class OrderItem extends Model {
 
+    static boot() {
+        super.boot()
+
+        this.addHook('beforeSave', 'OrderItemHook.updateSubtotal')
+        
+    }
+
     static get traits () {
         return ['App/Models/Traits/NoTimestamp']
-      }
+    }
+    
 
     // Relationship between OrderItem and Product
     product() {
