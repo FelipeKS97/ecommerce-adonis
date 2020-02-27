22dd64dedd2eed32c6bd66fa18fcaf5e3130cfb1
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-02-27 20:26:21 -0300

    Category CRUD implementation finished

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -44,6 +44,17 @@ class CategoryController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+
+    try {
+      const { title, description, image_id } = request.all()
+      const category = await Category.create({ title, description, image_id })
+      return response.status(201).send(category)
+    } catch (error) {
+
+      return response.status(400).send({
+        message: "Erro ao processar solicitação."
+      })
+    }
   }
 
   /**
@@ -55,7 +66,10 @@ class CategoryController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params: { id }, request, response, view }) {
+    const category = await Category.findOrFail(id)
+
+    return response.send(category)
   }
 
 
@@ -67,7 +81,15 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, request, response }) {
+    
+    const category = await Category.findOrFail(id)
+    const { title, description, image_id } = request.all()
+    category.merge({ title, description, image_id })
+    await category.save()
+
+    return response.send(category)
+
   }
 
   /**
@@ -78,7 +100,11 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    
+    const category = await Category.findOrFail(id)
+    await category.delete()
+    return response.status(204).send()
   }
 }
 
