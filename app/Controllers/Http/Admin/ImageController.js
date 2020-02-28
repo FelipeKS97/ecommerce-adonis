c217bf3d1c1edbc095c959467d7a3a63e278602c
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-02-28 18:06:04 -0300

    Image controller implementation finished

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -4,6 +4,14 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Image = use('App/Models/Image')
+const { 
+  manage_single_upload, 
+  manage_multiple_uploads
+} = use('App/Helpers')
+
+const fs = use('fs')
+
 /**
  * Resourceful controller for interacting with images
  */
@@ -18,8 +26,15 @@ class ImageController {
    * @param {View} ctx.view
    * @param {object} ctx.pagination
    */
-  async index ({ request, response, pagination }) {
+  async index ({ response, pagination }) {
 
+    const { page, limit } = pagination
+    const images = await Image
+      .query()
+      .orderBy('id', 'DESC')
+      .paginate(page, limit)
+    
+    return response.send(images)
     
   }
 
@@ -32,6 +47,67 @@ class ImageController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+
+    try {
+      const fileJar = request.file('images', {
+        types: ['image'],
+        size: '2mb'
+      })
+
+      let imagesArr = []
+
+      if(!fileJar.files) {
+        const file =  await manage_single_upload(fileJar)
+
+        if(file.moved()) {
+          const image = Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+
+          imagesArr.push(image)
+
+          return response
+            .status(201)
+            .send({
+              successes: imagesArr, 
+              errors:{} 
+            })
+
+        }
+
+        return response.status(400).send({
+          message:"Não foi possível processar a imagem no momento."
+        })
+      } 
+
+      let files = await manage_multiple_uploads(fileJar)
+
+      await Promise.all(
+        files.successes.map( async file => {
+          const image = Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+
+          imagesArr.push(image)
+        })
+      )
+      return response.status(201).send({ 
+        successes: imagesArr, 
+        errors: files.errors 
+      })
+
+    } catch (error) {
+
+      return response.status(400).send({
+        message: "Erro ao processar solicitação."
+      }) 
+    }
   }
 
   /**
@@ -43,7 +119,10 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response }) {
+  async show ({ params:{ id }, request, response }) {
+    const image = await Image.findOrFail(id)
+
+    return response.send(image)
   }
 
   /**
@@ -54,7 +133,23 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, request, response }) {
+
+    const image = await Image.findOrFail(id)
+
+    try {
+      image.merge(request.only(['original_name']))
+      await image.save()
+      
+      return response.send(image)
+      
+    } catch (error) {
+      
+      return response.status(400).send({
+        message: "Erro ao processar solicitação."
+      })    
+    }
+
   }
 
   /**
@@ -65,7 +160,26 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    
+    const image = await Image.findOrFail(id)
+
+    try {
+
+      let filepath = Helpers.publicPath(`uploads/${image.path}`)
+      
+      fs.unlink(filepath, err => {
+        if(!err) await image.delete()
+      })
+
+      return response.status(204).send()
+      
+    } catch (error) {
+      
+      return response.status(500).send({
+        message: "Erro ao processar solicitação."
+      })  
+    }
   }
 }
 
