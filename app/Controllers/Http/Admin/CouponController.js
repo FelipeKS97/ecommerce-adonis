9f23f277c226ce2b8089734a73c765e81736aa66
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-03-01 18:56:58 -0300

    CouponService implemented to sync relationships, CouponController CRUD methods implementation started

--- app/Controllers/Http/Admin/CouponController.js
+++ app/Controllers/Http/Admin/CouponController.js
@@ -4,6 +4,9 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Coupon = use('App/Models/Coupon')
+const Database = use('Database')
+
 /**
  * Resourceful controller for interacting with coupons
  */
@@ -16,8 +19,21 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+    const { page, limit } = pagination
+
+    const code = request.input('code')
+    const query = Coupon.query()
+
+    if(code) {
+      query.where('code', 'LIKE', `%${code}%`)
+    }
+
+    const coupons = await query.paginate(page, limit)
+
+    return response.send(coupons)
   }
 
 
@@ -30,6 +46,37 @@ class CouponController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+
+    const trx = await Database.beginTransaction()
+
+    let can_use_for = {
+      client: false,
+      product: false
+    }
+
+    try {
+      const couponData = request.only([
+        'code',
+        'discount',
+        'valid_from',
+        'valid_until',
+        'quantity',
+        'type',
+        'recursive'
+      ])
+      
+      const { users, products } = request.all()
+
+      const coupon = Coupon.create(couponData, trx)
+
+      // Service Layer starts
+
+      
+
+    } catch (error) {
+      
+    }
+
   }
 
   /**
@@ -41,7 +88,10 @@ class CouponController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params: { id }, request, response, view}) {
+    const coupon = await Coupon.findOrFail(id)
+
+    return response.send(coupon)
   }
 
   /**
@@ -63,7 +113,26 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    const trx = Database.beginTransaction()
+    const coupon = await Coupon.findOrFail(id)
+
+    try {
+      await coupon.products().detach([], trx)
+      await coupon.orders().detach([], trx)
+      await coupon.users().detach([], trx)
+      await coupon.delete()
+      await trx.commit()      
+
+      return response.status(204).send()
+      
+    } catch (error) {
+
+      await trx.rollback()
+      return response.status(400).send({
+        message: "Erro ao processar solicitação."
+      })  
+    }
   }
 }
 
