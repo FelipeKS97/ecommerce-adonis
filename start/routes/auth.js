159fcc941ccd895e5ae3b35e27bae92b1abb0775
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-03-05 18:44:27 -0300

    Adding middleware ACL verification into the auth route, removing unnecessary route /

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -4,16 +4,35 @@
 const Route = use('Route')
 
 Route.group(() => {
-    Route.post('register', 'AuthController.register').as('auth.register')
-    Route.post('login', 'AuthController.login').as('auth.login')
-    Route.post('refresh', 'AuthController.refresh').as('auth.refresh')
-    Route.post('logout', 'AuthController.logout').as('auth.logout')
+  Route.post('register', 'AuthController.register')
+    .as('auth.register')
+    .middleware(['guest'])
 
-    // Restore password methods:
+  Route.post('login', 'AuthController.login')
+    .as('auth.login')
+    .middleware(['guest'])
+    
+  Route.post('refresh', 'AuthController.refresh')
+    .as('auth.refresh')
+    .middleware(['guest'])
 
-    Route.post('reset-password', 'AuthController.forgot').as('auth.forgot')
-    Route.get('reset-password', 'AuthController.remember').as('auth.remember')
-    Route.put('reset-password', 'AuthController.reset').as('auth.reset')
+  Route.post('logout', 'AuthController.logout')
+    .as('auth.logout')
+    .middleware(['auth'])
 
-}).prefix('v1/auth')
-  .namespace('Auth')
+  // Restore password methods:
+
+  Route.post('reset-password', 'AuthController.forgot')
+    .as('auth.forgot')
+    .middleware(['guest'])
+
+  Route.get('reset-password', 'AuthController.remember')
+    .as('auth.remember')
+    .middleware(['guest'])
+
+  Route.put('reset-password', 'AuthController.reset')
+    .as('auth.reset')
+    .middleware(['guest'])
+})
+.prefix('v1/auth')
+.namespace('Auth')
--- start/routes/index.js
+++ start/routes/index.js
@@ -16,10 +16,6 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-}).as('home')
-
 require('./auth')
 require('./admin')
 require('./client')
