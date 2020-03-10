31a6ba4dfabb8c46d8b9d9d4012b39c95b1f0d72
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-03-10 18:43:27 -0300

    Dashboard/front-end controllers created and methods implemented successfully

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -6,43 +6,47 @@ const Route = use('Route')
 
 Route.group(() => {
 
-    // Categories resource methods:
+  // Categories resource methods:
 
-    Route.resource('categories', 'CategoryController')
-    .apiOnly()
-    .validator(new Map([
-      [['categories.store'], ['Admin/StoreCategory']],
-      [['categories.update'], ['Admin/UpdateCategory']],
-    ]))
+  Route.resource('categories', 'CategoryController')
+  .apiOnly()
+  .validator(new Map([
+    [['categories.store'], ['Admin/StoreCategory']],
+    [['categories.update'], ['Admin/UpdateCategory']],
+  ]))
 
-    // Products resource methods:
+  // Products resource methods:
 
-    Route.resource('products', 'ProductController').apiOnly()
+  Route.resource('products', 'ProductController').apiOnly()
 
-    // Images resource methods:
+  // Images resource methods:
 
-    Route.resource('images', 'ImageController').apiOnly()
+  Route.resource('images', 'ImageController').apiOnly()
 
-    // Users resource methods:
+  // Users resource methods:
 
-    Route.resource('users', 'UserController')
-    .apiOnly()
-    .validator(new Map([
-      [['users.store'], ['Admin/StoreUser']],
-      [['users.update'], ['Admin/StoreUser']],
-    ]))
+  Route.resource('users', 'UserController')
+  .apiOnly()
+  .validator(new Map([
+    [['users.store'], ['Admin/StoreUser']],
+    [['users.update'], ['Admin/StoreUser']],
+  ]))
 
-    // Coupon resource methods:
+  // Coupon resource methods:
 
-    Route.resource('coupons', 'CouponController').apiOnly()
+  Route.resource('coupons', 'CouponController').apiOnly()
 
-    // Orders resource methods:
-    
-    Route.post('orders/:id/discount', 'OrderController.applyDiscount')
-    Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
-    Route.resource('orders', 'OrderController')
-    .apiOnly()
-    .validator(new Map([[['orders.store'], ['Admin/StoreOrder']]]))
+  // Orders resource methods:
+  
+  Route.post('orders/:id/discount', 'OrderController.applyDiscount')
+  Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
+  Route.resource('orders', 'OrderController')
+  .apiOnly()
+  .validator(new Map([[['orders.store'], ['Admin/StoreOrder']]]))
+
+  // Dashboard route
+  
+  Route.get('dashboard', 'DashboardController.index').as('dashboard')
 
 }).prefix('v1/admin')
   .namespace('Admin')
